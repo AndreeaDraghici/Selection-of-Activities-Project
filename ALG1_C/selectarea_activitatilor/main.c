e068c6f11fb888629e33714f0ace2e1719228727
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-26 11:21:19 +0300

    Update main.c

--- ALG1_C/selectarea_activitatilor/main.c
+++ ALG1_C/selectarea_activitatilor/main.c
@@ -4,30 +4,6 @@
 #include "selectare.h"
 #include "selectare.c"
 
-#define NMAX 100
-
-int* activities( int start [NMAX],int duration [NMAX], int finish[NMAX],int  no_of_activity )
-{
-    int* arr=malloc(sizeof(int)*no_of_activity);  /*array arr of size n */
-    arr[0]=0; /* arr  start from index 1 */
-    arr[1]=duration [1];
-    arr[2]=duration[2];
-    int iterator;
-    int constant=1;
-    int aux=1;
-    for(iterator=1; iterator< no_of_activity; iterator ++)
-    {
-        if (start[iterator]  >=  finish[constant] )
-        {
-            aux=aux+1;
-            arr[aux]=arr[iterator];
-            printf("\n activity %d \n ",iterator);
-            constant=iterator;
-        }
-    }
-    arr[0]=aux; /* make the first element of arr equal to iterator */
-    return arr;
-}
 
 int main (int argc, char **argv)  /* rand function */
 {
@@ -43,15 +19,15 @@ int main (int argc, char **argv)  /* rand function */
     int *point= activities(duration,start,finish,no_of_activity);
     FILE *f , *g;
 
-    f=fopen("data/activity_in.in","r");
+    f=fopen("activity_in.in","r");
     fscanf(f,"%d",&no_of_activity);
 
-    g=fopen("data/activity_out.out","w");
+    g=fopen("activity_out.out","w");
 
     srand(time(NULL));
     for(add=0; add<40; add++)
 
-        fprintf(g,"%d ", randRange (duration));
+        fprintf(g,"%d ",randRange( duration));
 
     fclose(f);
     fclose(g);
